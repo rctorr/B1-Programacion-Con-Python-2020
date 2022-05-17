c56815879541efd0a91abf7be2f714348c07b832
rctorr <rictor@cuhrt.com>
2022-05-17 11:33:03 -0500

    Eliminando un archivo de prueba de mas en Ejemplo-02 Sesion-08

--- Sesion-08/Ejemplo-02/test_operaciones.py
+++
@@ -1,7 +0,0 @@
-import operaciones
-
-def test_suma_string():
-    resultado = operaciones.suma('Hola ', 'Mundo')
-    assert resultado == 'Hola Mundo'
-    assert type(resultado) is str
-    assert len(resultado) > 0
